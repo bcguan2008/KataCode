962c18fb8302796c3b3960d16ee11f7b8e9c765f
guanbingchang <guanbingchang@wanda.cn>
2016-07-24 19:49:08 +0800

    answer

--- quickstart1/javascript/framework.js
+++ quickstart1/javascript/framework.js
@@ -1,7 +1,42 @@
 /**
  * let's complete this framework
  */
-var myClass = function(){
-    
+
+var merge = function(obj,args){
+    for(var i in args){
+        obj[i] = args[i];
+    }
 }
-module.exports = myClass;
+
+var Class = function(args){
+    
+    var CONSTRUCTORKEY = "initialize";
+
+    var returnVal = function(){
+        if(typeof args[CONSTRUCTORKEY] ==='function'){
+            return args[CONSTRUCTORKEY].apply(this,arguments);
+        }
+    };
+
+    /**
+     * 提取merge方法
+     */
+    merge(returnVal.prototype,args);
+
+    returnVal.extend = function(childArgs){
+        var templateFun =function(){
+        };
+        templateFun.prototype = this.prototype ;
+        var child = function(){
+        };
+        
+        child.prototype.constructor = child;
+        
+        merge(child.prototype,childArgs);
+
+        return child;
+    }
+
+    return returnVal;
+}
+module.exports = Class;
