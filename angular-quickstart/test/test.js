67690f43df9123e1ee63f5d336598a8325b45ed4
guanbingchang <guanbingchang@wanda.cn>
2016-08-10 14:31:22 +0800

    update answer

--- angular-quickstart/test/test.js
+++ angular-quickstart/test/test.js
@@ -10,17 +10,42 @@ describe('Angular 类库',function(){
     describe('Angular 的依赖注入实现',function(){
         var app = angular.module();
 
-        it('angular.module 返回对象，包含factory',function(){
+        it('angular.module 返回对象，包含factory，injector',function(){
             angular.module.should.be.a('function');
             app.factory.should.be.a('function');
             app.injector.should.be.a('object');
         });
 
-        it('app.factory 链式调用',function(){
-            app.should.be.equal(app.factory('test',function(){}));
+        it('通过factory 注入对象，invoke调用实现',function(){
+            app.factory('baseVal',function(){
+                return {
+                    a:2,
+                    b:3
+                }
+            });
+
+            app.factory('minus',function(){
+                return function(a,b){
+                    return a-b;
+                }
+            });
+            
+            app.factory('plus',function(){
+                return function(a,b){
+                    return a+b;
+                }
+            });
+
+            var invoke = false; 
+            app.injector.invoke(function(baseVal,minus,plus){
+                invoke = true ;
+                expect(minus(baseVal.a,baseVal.b)).to.be.equal(-1);
+                expect(plus(baseVal.a,baseVal.b)).to.be.equal(5);
+            })
+            invoke.should.be.true;
         })
 
-        it('app 通过factory注入对象，invoke调用实现',function(){
+        it('app.factory 链式调用',function(){
             app.factory('baseVal',function(){
                 return {
                     a:2,
@@ -46,7 +71,7 @@ describe('Angular 类库',function(){
             });
 
             invoke.should.be.true;
-        });
+        })
 
     });
 
